901901c4be787aeb12a676c4fe0bd11f4c7aa5bf
KantoAndrianina <kanto.andrianina@yahoo.com>
2023-07-11 00:29:06 +0300

    modif template

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -22,9 +22,7 @@
     <div class="container-scroller">
       <div class="row p-0 m-0 proBanner" id="proBanner">
         <div class="col-md-12 p-0 m-0">
-          <div class="card-body card-body-padding d-flex align-items-center justify-content-between">
-            
-          </div>
+         
         </div>
       </div>
       <!-- partial:partials/_navbar.html -->
