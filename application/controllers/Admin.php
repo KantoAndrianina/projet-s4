f8a2c89d743f2652701fc8021c3c94f4a9549859
Colin <colinraminoson@gmail.com>
2023-07-11 15:21:42 +0300

    admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -31,6 +31,8 @@ class Admin extends CI_Controller
 	public function crud_liste()
 	{
 		$data = array();
+        $data['listePlat'] = $this->Model->getPlat();
+        $data['listeActivite'] = $this->Model->getPlat();
         $data['content'] = 'crud/listes';
 		$this->load->view('index',$data);
 	}
@@ -48,6 +50,7 @@ class Admin extends CI_Controller
 	}
 	public function insert()
 	{
+<<<<<<< HEAD
 		$descri = $this->input->post("descri");
 		$duree = $this->input->post("Duree");
 		$poidsDeb = $this->input->post("PoidsDeb");
@@ -58,6 +61,31 @@ class Admin extends CI_Controller
 		$this->Model->insertAchat($descri, $duree,$poidsDeb,$poidsFin,$nomActivite,$augmenter,$réduire);
 		$url=base_url().'index.php/admin/index';
 		redirect($url);
+=======
+		$id = $_GET['idPlat'];
+		$data = array();
+        $data['id'] = $id;
+        $data['content'] = 'crud/modif_plat';
+		$this->load->view('index',$data);
+	}
+	public function modifier_activites()
+	{
+		$id = $_GET['idPlat'];
+		$data = array();
+        $data['id'] = $id;
+        $data['content'] = 'crud/modif_activite';
+		$this->load->view('index',$data);
+	}
+	// public function insert()
+	// {
+	// 	$produit = $this->input->post("Nomplat");
+	// 	$quantite = $this->input->post("typePlat");
+	// 	$produit = $this->input->post("PrixUnitaire");
+	// 	$quantite = $this->input->post("ImgPlat");
+	// 	$this->Model->insertAchat($Nomplat, $typePlat,$PrixUnitaire,$ImgPlat);
+	// 	$url=base_url().'index.php/welcome/total';
+	// 	redirect($url);
+>>>>>>> parent of 28db2e9 (crud_modif_supp)
 		
 	}	
 }
